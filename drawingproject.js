fcba82fe36d46ad61bdbe25ea36bfaad3c06c3dc
Richard Tirado <richardtirado1@yahoo.com>
2016-10-05 09:02:05 -0700

    Changed the background

--- drawingproject.js
+++ drawingproject.js
@@ -8,13 +8,16 @@
   canvas.fillRect(2000000000, 300, 2000000056, 54545);
 
 //the sky
-canvas.fillStyle  = "aqua";
-canvas.fillRect(10, 10, 700, 290);
+var grd = canvas.createRadialGradient(75,50,50,90,100,900);
+grd.addColorStop(0,"aqua");
+grd.addColorStop(1,"white");
 
+canvas.fillStyle = grd;
+canvas.fillRect(10, 10, 1000, 290);
 
 //part of the ship a rectange
 canvas.fillStyle = "brown";
-canvas.fillRect(140, 275, 500, 50);
+canvas.fillRect(140, 275, 350, 30);
 
 //the sun
 canvas.beginPath();
@@ -25,17 +28,17 @@ canvas.fill();
 //half circle ,part of the ship
 canvas.beginPath();
 canvas.fillStyle = "brown";
-canvas.arc(390,315,200,3.1,0,true);
+canvas.arc(315,300,130,3.1,0,true);
 canvas.stroke();
 canvas.fill();
 //triangles on the sun
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.moveTo(90,50);
-canvas.lineTo(100,75);
-canvas.lineTo(100,25);
+canvas.lineTo(200,85);
+canvas.lineTo(280,45);
 canvas.fill();
 //pole on ship
 canvas.fillStyle = "black";
-canvas.fillRect(365, 50, 35, 245);
+canvas.fillRect(200, 50, 15, 245);
 //flag on ship
