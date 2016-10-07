ac602b11ea1e899602528d2e20a1231985158d32
Richard Tirado <richardtirado1@yahoo.com>
2016-10-07 09:16:51 -0700

    Finished the project thanks

--- drawingproject.js
+++ drawingproject.js
@@ -28,9 +28,65 @@ canvas.fillRect(200, 50, 15, 245);
  canvas.lineTo(80,60);
  canvas.lineTo(200,100);
  canvas.fill();
+ //half circle ,top part of island
+ canvas.beginPath();
+ canvas.fillStyle = "Khaki";
+ canvas.arc(960,470,260,35,20,true);
+ canvas.stroke();
+ canvas.fill();
  //base of the island
  canvas.fillStyle = "Khaki";
  canvas.fillRect(700, 400, 900, 170);
+
+//tree on island
+canvas.fillStyle = "BurlyWood";
+canvas.fillRect(950, 50, 15, 200);
+
+//leaves on palm tree
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(950,50);
+canvas.lineTo(950,80);
+canvas.lineTo(800,100);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(965,50);
+canvas.lineTo(965,80);
+canvas.lineTo(1100,100);
+canvas.fill();
+
+//coconuts on palm tree
+canvas.beginPath();
+canvas.fillStyle = "SaddleBrown";
+canvas.arc(945, 90, 10, 0, 2*Math.PI);
+canvas.stroke();
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "SaddleBrown";
+canvas.arc(960, 90, 10, 0, 2*Math.PI);
+canvas.stroke();
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "SaddleBrown";
+canvas.arc(800, 250, 10, 0, 2*Math.PI);
+canvas.stroke();
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "SaddleBrown";
+canvas.arc(1120, 250, 10, 0, 2*Math.PI);
+canvas.stroke();
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "SaddleBrown";
+canvas.arc(950, 250, 10, 0, 2*Math.PI);
+canvas.stroke();
+canvas.fill();
 
 //cannon holes on the ship
 canvas.fillStyle = "red";
